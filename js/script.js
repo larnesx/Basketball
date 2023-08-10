00899ea389d60e5fe22704c0b7ba403d1f9a05d0
Khushbakht <khushbakht.dev@gmail.com>
2023-08-10 11:06:00 +0500

    basketball-02

--- js/script.js
+++ js/script.js
@@ -2,6 +2,45 @@ const tabs = document.querySelectorAll('[data-tab]');
 const contents = document.querySelectorAll('[data-content]');
 const navButtons = document.querySelectorAll('.navbut');
 
+const typeWriterText = document.querySelector('.text-writer');
+const textArr = ['never\nstops', "is a gentleman sport", "is a knee-killin' sport"];
+
+let textArrayIndex = 0;
+let charIndex = 0;
+
+const type = () => {
+    if(charIndex <= textArr[textArrayIndex].length - 1) {
+        typeWriterText.textContent += textArr[textArrayIndex].charAt(charIndex);
+        charIndex++
+        setTimeout(type, 120);
+    } else {
+        setTimeout(erase, 800);
+    }
+}
+const erase = () => {
+    if(charIndex > 0) {
+        typeWriterText.textContent = textArr[textArrayIndex].slice(0, charIndex - 1);
+        charIndex--
+        setTimeout(erase, 60);
+    } else {
+        textArrayIndex++;
+        if(textArrayIndex >= textArr.length) {
+            textArrayIndex = 0;
+        }
+        setTimeout(type, 800);
+    }
+}
+
+//Starts animation on page load
+document.addEventListener('DOMContentLoaded', () => {
+    type()
+})
+
+
+
+
+
+
 tabs.forEach(function(item) {
     item.addEventListener('click', function() {
 
@@ -26,6 +65,4 @@ navButtons.forEach(function(item) {
         item.classList.add('navbutadd');
     })
 })
-
-
 
